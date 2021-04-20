0324dff616f501e6bfad75fb6b2a5a751ff40f16
Eric Balawejder <eric.balawejder@protonmail.com>
2021-04-20 14:19:39 -0400

    Added equals(), hashCode() and toString()

--- src/main/java/com/server/energy/resources/ApiResponse.java
+++ src/main/java/com/server/energy/resources/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.server.energy.resources;
 
+import java.util.Objects;
+
 public class ApiResponse {
 
     private final CalculationResult results;
@@ -10,6 +12,26 @@ public class ApiResponse {
 
     public CalculationResult getResults() {
         return new CalculationResult(results);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApiResponse that = (ApiResponse) o;
+        return Objects.equals(results, that.results);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(results);
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponse{" +
+                "results=" + results +
+                '}';
     }
 
 }
--- src/main/java/com/server/energy/resources/CalculationResult.java
+++ src/main/java/com/server/energy/resources/CalculationResult.java
@@ -1,5 +1,7 @@
 package com.server.energy.resources;
 
+import java.util.Objects;
+
 public class CalculationResult {
 
     private final Double energy;
@@ -21,6 +23,27 @@ public class CalculationResult {
 
     public String getUnits() {
         return units;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CalculationResult result = (CalculationResult) o;
+        return Objects.equals(energy, result.energy) && Objects.equals(units, result.units);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(energy, units);
+    }
+
+    @Override
+    public String toString() {
+        return "CalculationResult{" +
+                "energy=" + energy +
+                ", units='" + units + '\'' +
+                '}';
     }
 
 }
