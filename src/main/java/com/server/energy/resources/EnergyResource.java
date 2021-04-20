e87ef3ac4b34415fdb0316e35f4b9308151192a9
Eric Balawejder <eric.balawejder@protonmail.com>
2021-04-20 15:13:32 -0400

    Changed access modifiers for testing purposes

--- src/main/java/com/server/energy/resources/EnergyResource.java
+++ src/main/java/com/server/energy/resources/EnergyResource.java
@@ -50,7 +50,7 @@ public class EnergyResource {
     // Stream JSON file to handle huge input or slow connection. In this case the JSON needs
     // to be read as it comes from input part by part. The side effect is that you are able
     // to read corrupted JSON arrays in some kind of comfortable way.
-    private double energyMetered(JsonParser jsonParser, SensorReading sensor, int start, int end)
+    public double energyMetered(JsonParser jsonParser, SensorReading sensor, int start, int end)
             throws IOException {
 
         double sum = 0;
@@ -75,12 +75,12 @@ public class EnergyResource {
         return sum;
     }
 
-    private CalculationResult kilowattHour(double value) {
+    public static CalculationResult kilowattHour(double value) {
         final double kWh = (1.0 / 3600) * value;
         return new CalculationResult(round(kWh, 3), "kWh");
     }
 
-    private static double round(double value, int places) {
+    public static double round(double value, int places) {
         if (places < 0) {
             throw new IllegalArgumentException(places + " < 0");
         }
